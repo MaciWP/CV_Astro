335b3593022023860eb0477d46bf3e4028b8b90b
Oriol <oriolomb@gmail.com>
2025-05-30 18:16:18 +0200

    feat: add link prefetch component

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,26 @@
-import config from "./.eslintrc.json" assert { type: "json" };
-export default config;
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import astro from "eslint-plugin-astro";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,jsx,ts,tsx,astro}"],
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "jsx-a11y": jsxA11y,
+      astro,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    settings: { react: { version: "detect" } },
+  },
+];
